081fa70cd000404b956de5569ff3e009b00aeae9
Matthew Lichtenstein <mlicht@gatech.edu>
2017-03-03 17:27:08 -0500

    basic weapon physics

--- Assets/Scripts/Player2.cs
+++ Assets/Scripts/Player2.cs
@@ -10,10 +10,16 @@ public class Player2 : MonoBehaviour {
 
     private Rigidbody2D body;
     public bool isGrounded;
+    public GameObject potion;
+    private GameObject instance;
+    private Transform trans;
+    public bool thrown;
     // Use this for initialization
     void Start()
     {
         body = gameObject.GetComponent<Rigidbody2D>();
+        trans = gameObject.GetComponent<Transform>();
+        thrown = false;
     }
 
     // Update is called once per frame
@@ -29,6 +35,14 @@ public class Player2 : MonoBehaviour {
         } else if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(speed, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.E) && !thrown)
+        {
+            instance = (GameObject)Instantiate(potion, transform.localPosition + new Vector3(0.91f, 0.59f, 0), transform.rotation, trans);
+            instance.GetComponent<Transform>().parent = trans.parent;
+            instance.GetComponent<Rigidbody2D>().AddForce(new Vector2(160, 80));
+            instance.GetComponent<Rigidbody2D>().gravityScale = 1;
+            thrown = true;
         }
     }
 
---
+++ Assets/Scripts/WeaponQuality.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponQuality : MonoBehaviour {
+
+    private Rigidbody2D rigid;
+
+    public bool isPotion;
+    public Player2 player2;
+    public Player1 player1;
+    private Transform trans;
+
+    private bool thrust;
+    private int thrtimer;
+	// Use this for initialization
+	void Start () {
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+        thrtimer = 0;
+        thrust = false;
+        trans = gameObject.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isPotion && !thrust && Input.GetKeyDown(KeyCode.RightShift))
+        {
+            thrust = true;
+            thrtimer = 60;
+        }
+        if (thrust)
+        {
+            if (thrtimer > 30)
+            {
+                trans.Translate(2*Time.deltaTime, 0, 0);
+                thrtimer--;
+            }
+            else if (thrtimer > 0)
+            {
+                trans.Translate(-2 * Time.deltaTime, 0, 0);
+                thrtimer--;
+            } else
+            {
+                thrust = false;
+            }
+        }
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (isPotion)
+        {
+            Destroy(gameObject);
+            player2.thrown = false;
+        }
+    }
+
+    public Rigidbody2D getBody()
+    {
+        return rigid;
+    }
+}
