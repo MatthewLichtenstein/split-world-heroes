8c47b8d822dac5a6528c7ac4a3a0c4c1727ace60
Cheung, Ambrose Hin Wing <cheungambrose30@gmail.com>
2017-03-06 00:43:45 -0500

    Enemy Movement

--- Assets/Scripts/FlipScreen.cs
+++ Assets/Scripts/FlipScreen.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class FlipScreen : MonoBehaviour {
     public bool topScreen;
+    public float speed = 0.1f;
+    public Transform P1Transform;
+    public Transform P2Transform;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         topScreen = true;
 	}
 	
@@ -23,6 +26,18 @@ public class FlipScreen : MonoBehaviour {
                 gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 5f, 0);
                 topScreen = true;
             }
+        }
+
+        if(topScreen)
+        {
+            Vector3 direction = P1Transform.position - gameObject.transform.position;
+            var distance = direction.magnitude;
+            gameObject.transform.Translate(direction/distance * Time.deltaTime);
+        } else
+        {
+            Vector3 direction = P2Transform.position - gameObject.transform.position;
+            var distance = direction.magnitude;
+            gameObject.transform.Translate(direction / distance * Time.deltaTime);
         }
 	}
 }
