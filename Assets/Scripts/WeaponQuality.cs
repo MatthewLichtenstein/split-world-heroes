430b49e4861d08be73251fa497220162c66abbed
Cheung, Ambrose Hin Wing <cheungambrose30@gmail.com>
2017-03-06 20:28:57 -0500

    More Work

--- Assets/Scripts/WeaponQuality.cs
+++ Assets/Scripts/WeaponQuality.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponQuality : MonoBehaviour {
+public class WeaponQuality : MonoBehaviour
+{
 
     private Rigidbody2D rigid;
 
@@ -11,30 +12,64 @@ public class WeaponQuality : MonoBehaviour {
     public Player1 player1;
     private Transform trans;
 
-    
-	// Use this for initialization
-	void Start () {
-        rigid = gameObject.GetComponent<Rigidbody2D>();
-        trans = gameObject.GetComponent<Transform>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-		
-	}
-
-    void OnTriggerEnter2D(Collider2D coll)
+    private bool thrust;
+    private int thrtimer;
+    // Use this for initialization
+    void Start()
     {
-        
-        Destroy(gameObject);
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+        thrtimer = 0;
+        thrust = false;
+        trans = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isPotion && !thrust && Input.GetKeyDown(KeyCode.RightShift))
+        {
+            thrust = true;
+            thrtimer = 60;
+        }
+        if (thrust)
+        {
+            if (thrtimer > 30)
+            {
+                trans.Translate(2 * Time.deltaTime, 0, 0);
+                thrtimer--;
+            }
+            else if (thrtimer > 0)
+            {
+                trans.Translate(-2 * Time.deltaTime, 0, 0);
+                thrtimer--;
+            }
+            else
+            {
+                thrust = false;
+            }
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
         if (isPotion)
         {
+            Destroy(gameObject);
             player2.thrown = false;
+            if (other.transform.root.GetComponent<RedEnemy>())
+            {
+                Destroy(other.transform.gameObject);
+            }
         }
-        else
+        else if (other.transform.root.GetComponent<GreenEnemy>())
         {
-            player1.thrown = false;
+            Destroy(other.transform.gameObject);
+        }
+
+        if (other.transform.root.GetComponent<FlipScreen>())
+        {
+            Destroy(other.transform.gameObject);
         }
     }
 
