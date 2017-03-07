0f5df39fef555193c173b409e31d03bf8a4a45b1
Cheung, Ambrose Hin Wing <cheungambrose30@gmail.com>
2017-03-06 21:10:09 -0500

    Work

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+    public Canvas victoryScreen;
+    public bool done;
+
+    // Use this for initialization
+    void Start () {
+        done = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameObject.FindGameObjectsWithTag("Enemy") == null || GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
+        {
+            victoryScreen.gameObject.SetActive(true);
+            done = true;
+        }
+
+        if(done)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene("Game2");
+            }
+        }
+	}
+}
