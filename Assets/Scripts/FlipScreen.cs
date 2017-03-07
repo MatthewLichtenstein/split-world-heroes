79b3ce4beadfe1eb509ab22d37f052cbc98b3e7b
Cheung, Ambrose Hin Wing <cheungambrose30@gmail.com>
2017-03-06 20:28:31 -0500

    Work

--- Assets/Scripts/FlipScreen.cs
+++ Assets/Scripts/FlipScreen.cs
@@ -2,56 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlipScreen : MonoBehaviour {
+public class FlipScreen : MonoBehaviour
+{
     public bool topScreen;
     public float speed = 0.1f;
     public Transform P1Transform;
     public Transform P2Transform;
+    public Canvas gameOverScreen;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         topScreen = true;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.Space))
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             if (topScreen)
             {
                 gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 5f, 0);
                 topScreen = false;
-            } else
+            }
+            else
             {
                 gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 5f, 0);
                 topScreen = true;
             }
         }
 
-        if(topScreen)
+        if (topScreen)
         {
-            if(P1Transform)
+            if (P1Transform)
             {
                 Vector3 direction = P1Transform.position - gameObject.transform.position;
                 var distance = direction.magnitude;
-                gameObject.transform.Translate(direction/distance * Time.deltaTime);
+                gameObject.transform.Translate(direction / distance * Time.deltaTime);
             }
-        } else
+        }
+        else
         {
-            if(P2Transform)
+            if (P2Transform)
             {
                 Vector3 direction = P2Transform.position - gameObject.transform.position;
                 var distance = direction.magnitude;
                 gameObject.transform.Translate(direction / distance * Time.deltaTime);
             }
         }
-	}
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.transform.root.GetComponent<Player1>() || other.transform.root.GetComponent<Player2>())
         {
             Destroy(other.transform.gameObject);
+            gameOverScreen.gameObject.SetActive(true);
         }
     }
 }
